2175af71c21accfa33b78ee4fdc4d7da7b647a5b
Alex L L Cancado <alxcancado@proton.me>
2025-05-05 16:15:16 -0300

    feat: update dependencies and add new UI components
    - Updated Prisma and related dependencies to version 5.19.1.
    - Introduced a new Badge component for displaying product status.
    - Added a DataTable component for rendering tabular data using @tanstack/react-table.
    - Created a Table component with various subcomponents for better structure and styling.
    - Implemented a Prisma client with extended functionality for product status computation.
    - Defined product table columns with status badges for better visual representation.
    - Added a Docker Compose configuration for PostgreSQL setup.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,34 @@
-const ProductsPage = () => {
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
+
+const ProductsPage = async () => {
+  // isso aqui é um server component, portanto podemos fazer chamadas a API diretamente aqui
+  // não é o ideal, mas é só um exemplo
+  // const products = await fetch('https://dummyjson.com/products').then((res) => res.json());
+  // como é um server component, consigo transformar em função async e fazer chamadas a API diretamente aqui
+  const products = await db.product.findMany();
   return (
-    <div>products page</div>
-  )
-}
+    <div className="m-4 w-full space-y-4 rounded-lg bg-white p-4">
+      {/* Esquerda */}
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        {/* Direita */}
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo Produto
+        </Button>
+      </div>
+      <DataTable columns={productTableColumns} data={products} />
+    </div>
+  );
+};
 
 export default ProductsPage;
